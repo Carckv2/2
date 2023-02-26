88d397bc146afba4805c46f13239956d71d3223b
Carckv2 <Ankitshresthaas2@mail.com>
2023-02-26 15:35:04 -0500

    first commit

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# kernel
+git clone https://github.com/Carckv2/android_kernel_samsung_j8y18lte.git -b lineage-18.1 kernel/samsung/j8y18lte
+
+# vendor 
+git clone https://github.com/goldfish07/android_vendor_samsung_j8y18lte.git -b 10-64 vendor/samsung/j8y18lte
